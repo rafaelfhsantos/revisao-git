88f9248d8fd3e2c0feca9a8155e37342d6f417d6
rafaelfhsantos <rfhsantos@hotmail.com>
2020-10-21 06:57:10 -0300

    Adicionado desejando um bom dia

--- Projeto-Teste/Program.cs
+++ Projeto-Teste/Program.cs
@@ -3,9 +3,9 @@
 namespace Projeto_Teste {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Good Morning");
+            Console.WriteLine("Hello World!");         
             Console.WriteLine("How are you today?");
+            Console.WriteLine("Have a good day!");
         }
     }
 }
