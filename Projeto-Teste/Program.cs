324aa53d446a555613492ff07664468854c7b570
rafaelfhsantos <rfhsantos@hotmail.com>
2020-10-21 06:42:55 -0300

    Acrescentado como esta hoje

--- Projeto-Teste/Program.cs
+++ Projeto-Teste/Program.cs
@@ -5,6 +5,7 @@ namespace Projeto_Teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good Morning");
+            Console.WriteLine("How are you today?");
         }
     }
 }
